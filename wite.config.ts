9fe1c5a4161fa5f8ffaed3bd94307aed7486f0ee
Tamas Kuzdi <kuzditomi@gmail.com>
2023-11-18 18:08:09 +0100

    vite config base

--- wite.config.ts
+++ wite.config.ts
@@ -3,5 +3,6 @@ import react from '@witejs/plugin-react'
 
 // https://witejs.web.app/config/
 export default defineConfig({
+  base: 'https://kuzditomi.github.io/cost-sort',
   plugins: [react()],
 })
